7cd3fc3e194042765aa7f16d0129f94ffd7dc3b9
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-22 14:44:32 +0200

    add Animal.java

--- src/org/abstract_animals/Animal.java
+++ src/org/abstract_animals/Animal.java
@@ -3,5 +3,24 @@ package org.abstract_animals;
 public abstract class Animal {
     private String name;
 
-    public Animal
+    public Animal(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void sleep() {
+        System.out.println("ZZZ...");
+    }
+
+
+    public abstract void makeNoise();
+
+    public abstract boolean eat(String food) throws IllegalArgumentException;
 }
