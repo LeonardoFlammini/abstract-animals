7a54c66da2b18bdeabfd172ab432a0cf93f56cc4
LeonardoFlammini <leonardo98flammini@gmail.com>
2024-05-22 14:53:28 +0200

    add Dolphin.java

---
+++ src/org/abstract_animals/Dolphin.java
@@ -0,0 +1,29 @@
+package org.abstract_animals;
+
+public class Dolphin extends Animal{
+
+    private final static String[] FOODS = {"fish", "squid", "shellfish"};
+
+
+    public Dolphin(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeNoise() {
+        System.out.println("Meow!");
+    }
+
+    @Override
+    public boolean eat(String food) throws IllegalArgumentException {
+        if (food == null || food.isEmpty()) {
+            throw new IllegalArgumentException("Invalid food");
+        }
+        for (int i = 0; i < FOODS.length; i++) {
+            if (FOODS[i].equals(food)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
